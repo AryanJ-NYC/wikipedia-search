53c4be7390df4775612196daf53e39bf3e2597e2
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2015-12-05 14:16:15 -0500

    Fetch data from Wikipedia correctly

--- app/app.js
+++ app/app.js
@@ -1,1 +1,17 @@
 app = angular.module('wikiSearchApp', []);
+
+app.controller('wikiController', ['$scope', '$http', function($scope, $http) {
+    var apiBaseUrl = 'https://en.wikipedia.org/w/api.php';
+    $scope.searchValue = 'albert';
+
+    $http.jsonp(apiBaseUrl, {params: {
+        action: 'query',
+        list: 'search',
+        format: 'json',
+        srsearch: $scope.searchValue,
+        callback: 'JSON_CALLBACK'
+    }})
+    .then(function (response) {
+        $scope.searchResults = response.data;
+    });
+}]);
