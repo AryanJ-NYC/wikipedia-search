f997cb3084499dffdd972dac6b5ebd2eab08347f
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2015-12-07 01:11:01 -0500

    Auto-update as user types into search box

--- app/app.js
+++ app/app.js
@@ -2,16 +2,19 @@ app = angular.module('wikiSearchApp', []);
 
 app.controller('wikiController', ['$scope', '$http', function($scope, $http) {
     var apiBaseUrl = 'https://en.wikipedia.org/w/api.php';
-    $scope.searchValue = 'albert';
 
-    $http.jsonp(apiBaseUrl, {params: {
-        action: 'query',
-        list: 'search',
-        format: 'json',
-        srsearch: $scope.searchValue,
-        callback: 'JSON_CALLBACK'
-    }})
-    .then(function (response) {
-        $scope.searchResults = response.data;
-    });
+    $scope.onTypeEvent = function() {
+        $http.jsonp(apiBaseUrl, {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    format: 'json',
+                    srsearch: $scope.searchValue,
+                    callback: 'JSON_CALLBACK'
+                }
+            })
+            .then(function (response) {
+                $scope.searchResults = response.data;
+            });
+    }
 }]);
