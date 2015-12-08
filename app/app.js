e05151ee23d087fc7d38e6e7025b9329a1ad9e11
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2015-12-07 20:02:03 -0500

    Random article link created Close #2

--- app/app.js
+++ app/app.js
@@ -3,8 +3,27 @@ app = angular.module('wikiSearchApp', ['ngSanitize']);
 app.controller('wikiController', ['$scope', '$http', function($scope, $http) {
     var apiBaseUrl = 'https://en.wikipedia.org/w/api.php';
 
-    $scope.onTypeEvent = function() {
-        $http.jsonp(apiBaseUrl, {
+    angular.element(document).ready(function () {
+        $http.jsonp(apiBaseUrl,
+            {
+                params: {
+                    action: 'query',
+                    list: 'random',
+                    rnlimit: '1',
+                    rnnamespace: '0',
+                    format: 'json',
+                    callback: 'JSON_CALLBACK'
+                }
+            })
+            .then(function (response) {
+                $scope.randomLink = "http://en.wikipedia.org/?curid=" + response.data.query.random[0].id;
+            }
+        );
+    });
+
+    $scope.onTypeEvent = function () {
+        $http.jsonp(apiBaseUrl,
+            {
                 params: {
                     action: 'query',
                     list: 'search',
@@ -16,5 +35,5 @@ app.controller('wikiController', ['$scope', '$http', function($scope, $http) {
             .then(function (response) {
                 $scope.searchResults = response.data.query.search;
             });
-    }
+    };
 }]);
