0f97eb086c42d1602ac680ea165370b3fae54cc8
Aryan Jabbari <Aryan.Jabbari21@qmail.cuny.edu>
2016-07-28 10:43:54 -0400

    Add styling to Random Wikipedia article button

--- app/app.js
+++ app/app.js
@@ -1,39 +1,42 @@
-app = angular.module('wikiSearchApp', ['ngSanitize']);
+let app = angular.module('wikiSearchApp', ['ngSanitize']);
 
 app.controller('wikiController', ['$scope', '$http', function($scope, $http) {
-    var apiBaseUrl = 'https://en.wikipedia.org/w/api.php';
+  let apiBaseUrl = 'https://en.wikipedia.org/w/api.php';
 
-    angular.element(document).ready(function () {
-        $http.jsonp(apiBaseUrl,
-            {
-                params: {
-                    action: 'query',
-                    list: 'random',
-                    rnlimit: '1',
-                    rnnamespace: '0',
-                    format: 'json',
-                    callback: 'JSON_CALLBACK'
-                }
-            })
-            .then(function (response) {
-                $scope.randomLink = "http://en.wikipedia.org/?curid=" + response.data.query.random[0].id;
+  angular.element(document).ready(function () {
+    $http.jsonp(apiBaseUrl,
+      {
+          params: {
+              action: 'query',
+              list: 'random',
+              rnlimit: '1',
+              rnnamespace: '0',
+              format: 'json',
+              callback: 'JSON_CALLBACK'
+          }
+      })
+      .then(function (response) {
+          $scope.randomLink = "http://en.wikipedia.org/?curid=" + response.data.query.random[0].id;
+      }
+    );
+  });
+
+  $scope.onTypeEvent = function () {
+    if ($scope.searchValue == '') $scope.searchResults = [];
+    else {
+      $http.jsonp(apiBaseUrl,
+          {
+            params: {
+                action: 'query',
+                list: 'search',
+                format: 'json',
+                srsearch: $scope.searchValue,
+                callback: 'JSON_CALLBACK'
             }
-        );
-    });
-
-    $scope.onTypeEvent = function () {
-        $http.jsonp(apiBaseUrl,
-            {
-                params: {
-                    action: 'query',
-                    list: 'search',
-                    format: 'json',
-                    srsearch: $scope.searchValue,
-                    callback: 'JSON_CALLBACK'
-                }
-            })
-            .then(function (response) {
-                $scope.searchResults = response.data.query.search;
-            });
-    };
+          })
+          .then(function (response) {
+              $scope.searchResults = response.data.query.search;
+          });
+    }
+  };
 }]);
